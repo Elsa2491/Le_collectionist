27dc0f8ff4b0b713678356f8abf5d45b3707549a
Elsa Touma elsa.touma@gmail.com <elsa.touma@gmail.com>
2022-09-02 15:51:15 +0200

    Shop model: minimal actions

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+Shop.destroy_all
+puts 'Database cleaned'
+
+
+puts 'Creating user'
+
+elsa = User.create!(email: 'lecollectionist@test.com', password: 'lecollectionist_test')
+
+puts 'Creating shops'
+
+leroy_merlin = Shop.create!(user: elsa, name: "Leroy Merlin")
+maison_du_monde = Shop.create!(user: elsa, name: "Maison du monde")
+emma = Shop.create!(user: elsa, name: "Emma")
+swarovski = Shop.create!(user: elsa, name: "Swarovski")
+bo_mie = Shop.create!(user: elsa, name: "Bo&mie")
+
+puts 'Shops created'
