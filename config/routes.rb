f11088c228bcd8c47468e55d68d597553cbb3db8
Elsa Touma elsa.touma@gmail.com <elsa.touma@gmail.com>
2022-09-07 17:47:37 +0200

    remove useless method

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :shops, only: [:index, :show, :edit, :update]
+  resources :shops, only: [:index, :edit, :update]
   root to: 'shops#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
