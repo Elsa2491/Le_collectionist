55aba52e2db575b5d3b3983867643507e3aa06a6
Elsa Touma elsa.touma@gmail.com <elsa.touma@gmail.com>
2022-09-08 15:06:06 +0200

    Launch rubocop

--- app/controllers/sites_controller.rb
+++ app/controllers/sites_controller.rb
@@ -2,7 +2,11 @@ class SitesController < ApplicationController
   def change_locale
     @locale = I18n.available_locales.map(&:to_s).include?(params[:locale]) ? params[:locale] : I18n.default_locale
 
-    referer = request.referer && (Rails.application.routes.recognize_path(request.referer) rescue nil)
+    referer = request.referer && begin
+      Rails.application.routes.recognize_path(request.referer)
+    rescue StandardError
+      nil
+    end
     redirect_to (referer && url_for(referer.merge({ locale: @locale }))) || root_path({ locale: @locale })
   end
 end
